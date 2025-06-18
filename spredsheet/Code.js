40d4331198f70322bc229b0cc5c06db68c741372
networdSorcerer <gigattfyhig@gmail.com>
2025-06-18 13:31:32 +0900

    google

--- spredsheet/Code.js
+++ spredsheet/Code.js
@@ -1,6 +1,23 @@
 function myFunction() {
-  Logger.log('Hello World');
-  Logger.log(SpreadsheetApp.getActiveSheet().getRange(1, 2).getValue());
-  Logger.log(SpreadsheetApp.getActiveSheet().getRange(3, 2).getValue());
-  Logger.log(SpreadsheetApp.getActiveSheet().getRange(4, 3).getValue());
+  Logger.log(
+    "Hello World"
+  )
+  Logger.log(SpreadsheetApp.getActiveSheet().getRange(1,2).getValue());
+  Logger.log(SpreadsheetApp.getActiveSheet().getRange(3,2).getValue());
+  Logger.log(SpreadsheetApp.getActiveSheet().getRange(4,3).getValue());
+
+  let name 
+  name = SpreadsheetApp.getActiveSheet().getRange(4,3).getValue()
+  Logger.log(name)
+
+  let email
+  email = SpreadsheetApp.getActiveSheet().getRange(4,2).getValue()
+  Logger.log(email)
+  
+  let numOfmem
+  numOfmem = SpreadsheetApp.getActiveSheet().getRange(2,4).getValue()
+  let factor = 2
+  Logger.log(numOfmem + factor)
+
+
 }
