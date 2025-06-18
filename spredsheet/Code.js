de00a9be51b21ff436ba2f29d0c3e7f8389eb441
networdSorcerer <gigattfyhig@gmail.com>
2025-06-18 13:15:50 +0900

    google

--- spredsheet/Code.js
+++ spredsheet/Code.js
@@ -1,5 +1,6 @@
 function myFunction() {
-  Logger.log(
-    "Hello World"
-  )
+  Logger.log('Hello World');
+  Logger.log(SpreadsheetApp.getActiveSheet().getRange(1, 2).getValue());
+  Logger.log(SpreadsheetApp.getActiveSheet().getRange(3, 2).getValue());
+  Logger.log(SpreadsheetApp.getActiveSheet().getRange(4, 3).getValue());
 }
