5c12271a06bba53911ad57c48dddce9d4cb19016
networdSorcerer <gigattfyhig@gmail.com>
2025-06-18 13:56:06 +0900

    google

--- spredsheet/Code.js
+++ spredsheet/Code.js
@@ -1,23 +1,42 @@
 function myFunction() {
-  Logger.log(
-    "Hello World"
-  )
-  Logger.log(SpreadsheetApp.getActiveSheet().getRange(1,2).getValue());
-  Logger.log(SpreadsheetApp.getActiveSheet().getRange(3,2).getValue());
-  Logger.log(SpreadsheetApp.getActiveSheet().getRange(4,3).getValue());
+  Logger.log('Hello World');
+  Logger.log(SpreadsheetApp.getActiveSheet().getRange(1, 2).getValue());
+  Logger.log(SpreadsheetApp.getActiveSheet().getRange(3, 2).getValue());
+  Logger.log(SpreadsheetApp.getActiveSheet().getRange(4, 3).getValue());
 
-  let name 
-  name = SpreadsheetApp.getActiveSheet().getRange(4,3).getValue()
-  Logger.log(name)
+  let name;
+  name = SpreadsheetApp.getActiveSheet().getRange(4, 3).getValue();
+  let role = SpreadsheetApp.getActiveSheet().getRange(4, 4).getValue();
 
-  let email
-  email = SpreadsheetApp.getActiveSheet().getRange(4,2).getValue()
-  Logger.log(email)
-  
-  let numOfmem
-  numOfmem = SpreadsheetApp.getActiveSheet().getRange(2,4).getValue()
-  let factor = 2
-  Logger.log(numOfmem + factor)
+  Logger.log(name);
+  let mainTitle = name + role + ' 안녕하세요';
+  Logger.log(mainTitle);
 
+  let email;
+  email = SpreadsheetApp.getActiveSheet().getRange(4, 2).getValue();
+  Logger.log(email);
 
+  let numOfmem;
+  numOfmem = SpreadsheetApp.getActiveSheet().getRange(2, 4).getValue();
+  let factor = 2;
+  Logger.log(numOfmem + factor);
+
+  Logger.log(3 * 5);
+  Logger.log(3 + 5);
+
+  let one = 1;
+  let two = 2;
+
+  Logger.log(one + two);
+  Logger.log(typeof two);
+
+  let name2 = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('후원자정보').getRange(4, 3).getValue();
+  let role2 = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('후원자정보').getRange(4, 4).getValue();
+
+  // let mailTitle = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("이메일내용").getRange(3, 3).getValue();
+
+  let mailTitle2 = name2 + ' ' + role2 + '님. 반갑습니다. ';
+  let mailBody = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('이메일내용').getRange(3, 4).getValue();
+
+  Logger.log(mailTitle2 + ' ' + mailBody);
 }
